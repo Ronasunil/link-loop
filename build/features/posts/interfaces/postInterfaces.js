660398c6863d061c7ae1113d9279b4fe61a829f3
Ronasunil <ronasunilcoc@gmail.com>
2024-10-08 01:30:55 +0530

    Save local changes before revert

--- build/features/posts/interfaces/postInterfaces.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.privacyEnum = void 0;
-var privacyEnum;
-(function (privacyEnum) {
-    privacyEnum["PUBLIC"] = "public";
-    privacyEnum["PRIVATE"] = "private";
-    privacyEnum["UNLISTED"] = "unlisted";
-})(privacyEnum || (exports.privacyEnum = privacyEnum = {}));
